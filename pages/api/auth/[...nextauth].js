ec77dd16b438d72b90444bfda737085cedd9994c
atilla tanrikulu <tamsar@atillas-MacBook-Pro.local>
2025-05-11 00:35:49 +0300

    fix: add full Drive scope to fix script copy issue

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -52,6 +52,7 @@ export const authOptions = {
             "openid",
             "https://www.googleapis.com/auth/userinfo.email",
             "https://www.googleapis.com/auth/userinfo.profile",
+            "https://www.googleapis.com/auth/drive", // Full Drive access scope
             "https://www.googleapis.com/auth/drive.file",
             "https://www.googleapis.com/auth/drive.readonly",
             "https://www.googleapis.com/auth/spreadsheets",
